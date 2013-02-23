269ed3db07eaa190c43db54385166be5e5044189
Shivalik Narad <shivaliknarad@gmail.com>
2013-02-23 02:00:32 -0800

    3rd commit (created Hello_World)

---
+++ Hello_World/Hello_World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello_World
+//
+//  Created by Admin on 2/23/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
