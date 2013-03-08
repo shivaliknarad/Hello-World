609ecce9107a1a0782eebef41690b355e383a842
Shivalik Narad <shivaliknarad@gmail.com>
2013-03-08 14:41:50 -0800

    5th commit ( added interface)

--- Hello_World/Hello_World/main.m
+++ Hello_World/Hello_World/main.m
@@ -8,16 +8,48 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
+//-----interface-----
+@interface Person : NSObject
 {
-
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        NSLog(@"This just a test run for the 4th commit.");
-        
-    }
-    return 0;
+    int age;
+    int weight;
 }
+-(void) print;
+-(void) setAge: (int) a;
+-(void) setWeight: (int) w;
+@end
 
+//-----implementation-----
+@implementation Person
+-(void) print
+{
+    //commit was changed today
+    NSLog(@"This is the 6th commit");
+    NSLog(@"I am %i years old and my weight is %i pounds", age, weight);
+}
+-(void) setAge: (int) a
+{
+    age = a;
+}
+-(void) setweight: (int) w
+{
+    weight = w;
+}
+@end
+
+int main(int argc, char *argV[])
+{
+    @autoreleasepool
+    {
+        Person *shivalik;
+        
+        shivalik = [Person alloc];// hey we got some object and we need to reserve some memory for it
+        shivalik = [shivalik init];//initializes the object and makes it usable
+        
+        [shivalik setAge: 21];
+        [shivalik setweight:120];
+        [shivalik print];
+        
+        return 0;
+    }
+}
